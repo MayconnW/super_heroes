2f10d6f0a8ca733d03478d660aa0e303ecb02512
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-04-10 18:49:57 -0300

    Adjusting global styles

--- src/styles/global.js
+++ src/styles/global.js
@@ -6,24 +6,19 @@ const Global = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     outline: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-    color: #565656;
+    font-family: 'Roboto', sans-serif;
   }
 
   body {
     text-rendering: optimizeLegibility;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    font-family: "Montserrat", sans-serif;
+    font-family: "Roboto", sans-serif;
     line-height: 1.5;
     font-weight: normal;
     flex: 1;
     display: flex;
     flex-direction: column;
-    .ReactModalPortal {
-      z-index: 10;
-    }
   }
 
   html {
